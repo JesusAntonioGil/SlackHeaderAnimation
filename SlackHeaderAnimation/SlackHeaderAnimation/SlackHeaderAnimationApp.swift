765f2f67bd768823443983fa358f722b12d2ad81
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-04-10 12:06:00 +0200

    Create project

---
+++ SlackHeaderAnimation/SlackHeaderAnimation/SlackHeaderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackHeaderAnimationApp.swift
+//  SlackHeaderAnimation
+//
+//  Created by Jesus Antonio Gil on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SlackHeaderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
