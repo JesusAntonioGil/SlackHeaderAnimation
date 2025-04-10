f32a6d0820624af0a6ca703e7860778bb2eb1d0a
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-04-10 13:53:09 +0200

    Project done

--- SlackHeaderAnimation/SlackHeaderAnimation/ContentView.swift
+++ SlackHeaderAnimation/SlackHeaderAnimation/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Namespace private var animation
+    
     var body: some View {
         VStack {
             HStack(spacing: 10) {
@@ -15,24 +17,50 @@ struct ContentView: View {
                     .font(.title3)
                 
                 PopOutView { isExpanded in
-                    HStack {
-                        Image(systemName: "number")
-                            .fontWeight(.semibold)
+                    HStack(spacing: 10) {
+                        ZStack {
+                            if !isExpanded {
+                                Image(systemName: "number")
+                                    .fontWeight(.semibold)
+                                    .matchedGeometryEffect(id: "#", in: animation)
+                            }
+                        }
+                        .frame(width: 20)
                         
                         VStack(alignment: .leading, spacing: 4) {
-                            Text("general")
-                                .fontWeight(.semibold)
+                            HStack(spacing: 0) {
+                                if isExpanded {
+                                    Image(systemName: "number")
+                                        .fontWeight(.semibold)
+                                        .matchedGeometryEffect(id: "#", in: animation)
+                                        .scaleEffect(0.8)
+                                }
+                                
+                                Text("general")
+                            }
+                            .fontWeight(.semibold)
                             
                             Text("36 Members - 4 Online")
                                 .font(.caption)
                                 .foregroundStyle(.gray)
                         }
+                        .offset(x: isExpanded ? -30 : 0)
                     }
                     .padding(.horizontal, 10)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .frame(height: 50)
                 } content: { isExpanded in
-                    
+                    VStack(spacing: 12) {
+                        CustomButton("message", title: "Messages")
+                        CustomButton("note.text", title: "#general")
+                        CustomButton("square.3.stack.3d.top.fill", title: "Files")
+                        CustomButton("folder", title: "Bookmarks")
+                        
+                        Divider()
+                        
+                        CustomButton("text.book.closed", title: "Members")
+                        CustomButton("gearshape", title: "Settings & Details")
+                    }
                 }
 
                 Image(systemName: "airpods.max")
@@ -42,6 +70,28 @@ struct ContentView: View {
             Spacer()
         }
         .padding(15)
+    }
+    
+    
+    @ViewBuilder
+    private func CustomButton(_ image: String, title: String, action: @escaping () -> () = { }) -> some View {
+        Button(action: action) {
+            HStack(spacing: 12) {
+                Image(systemName: image)
+                    .frame(width: 25)
+                
+                Text(title)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 5)
+            .foregroundStyle(Color.primary)
+        }
     }
 }
 
--- SlackHeaderAnimation/SlackHeaderAnimation/Helpers/PopOutView.swift
+++ SlackHeaderAnimation/SlackHeaderAnimation/Helpers/PopOutView.swift
@@ -15,11 +15,13 @@ struct PopOutView<Header: View, Content: View>: View {
     @State private var sourceRect: CGRect = .zero
     @State private var showFullScreenCover: Bool = false
     @State private var animateView: Bool = false
+    // Haptics
+    @State private var haptics: Bool = false
     
     
     var body: some View {
         header(animateView)
-            .background(.gray.opacity(0.1))
+            .background(solidBackground(color: .gray, opacity: 0.1))
             .clipShape(.rect(cornerRadius: 10))
             .onGeometryChange(for: CGRect.self) {
                 $0.frame(in: .global)
@@ -27,7 +29,9 @@ struct PopOutView<Header: View, Content: View>: View {
                 sourceRect = newValue
             }
             .contentShape(.rect)
+            .opacity(showFullScreenCover ? 0 : 1)
             .onTapGesture {
+                haptics.toggle()
                 toogleFullScreenCover()
             }
             .fullScreenCover(isPresented: $showFullScreenCover) {
@@ -37,10 +41,14 @@ struct PopOutView<Header: View, Content: View>: View {
                     header: header,
                     content: content
                 ) {
-                    
+                    withAnimation(.easeInOut(duration: 0.25), completionCriteria: .removed) {
+                        animateView = false
+                    } completion: {
+                        toogleFullScreenCover()
+                    }
                 }
             }
-
+            .sensoryFeedback(.impact, trigger: haptics)
     }
     
     
@@ -64,29 +72,96 @@ fileprivate struct PopOutOverlay<Header: View, Content: View>: View {
     var dismissView: () -> ()
     // View Properties
     @State private var edgeInsets: EdgeInsets = .init()
+    @State private var scale: CGFloat = 1
     
     
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
-            header(animateView)
+            HStack(spacing: 10) {
+                if animateView {
+                    Button(action: dismissView) {
+                        Image(systemName: "xmark")
+                            .foregroundStyle(Color.primary)
+                            .contentShape(.rect)
+                    }
+                }
+                
+                header(animateView)
+            }
+            
+            if animateView {
+                content(animateView)
+                    .transition(.blurReplace)
+            }
         }
+        .frame(maxWidth: animateView ? .infinity : nil)
+        .padding(animateView ? 15 : 0)
+        .background {
+            ZStack {
+                solidBackground(color: .gray, opacity: 0.1)
+                    .opacity(!animateView ? 1 : 0)
+                
+                Rectangle()
+                    .fill(.background)
+                    .opacity(animateView ? 1 : 0)
+            }
+        }
+        .clipShape(.rect(cornerRadius: animateView ? 20 : 10))
+        .scaleEffect(scale, anchor: .top)
+        .frame(
+            width: animateView ? nil : sourceRect.width,
+            height: animateView ? nil : sourceRect.height
+        )
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .offset(
-            x: sourceRect.minX,
-            y: sourceRect.minY
+            x: animateView ? 0 : sourceRect.minX,
+            y: animateView ? 0 : sourceRect.minY
         )
+        .padding(animateView ? 15 : 0)
+        .padding(.top, animateView ? edgeInsets.top : 0)
         .ignoresSafeArea()
         .presentationBackground {
-            Rectangle()
-                .fill(.black.opacity(animateView ? 0.5 : 0))
-                .onTapGesture {
-                    dismissView()
-                }
+            GeometryReader {
+                let size = $0.size
+                
+                Rectangle()
+                    .fill(.black.opacity(animateView ? 0.5 : 0))
+                    .onTapGesture {
+                        dismissView()
+                    }
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged({ value in
+                                let height = value.translation.height
+                                let scale = height / size.height
+                                let applyingRatio: CGFloat = 0.1
+                                self.scale = 1 + (scale * applyingRatio)
+                            }).onEnded({ value in
+                                let velocityHeight = value.velocity.height / 5
+                                let height = value.translation.height + velocityHeight
+                                let scale = height / size.height
+                                
+                                withAnimation(.easeInOut(duration: 0.25)) {
+                                    self.scale = 1
+                                }
+                                
+                                if -scale > 0.5 {
+                                    dismissView()
+                                }
+                            })
+                    )
+            }
         }
         .onGeometryChange(for: EdgeInsets.self) {
             $0.safeAreaInsets
         } action: { newValue in
+            guard !animateView else { return }
             edgeInsets = newValue
+            Task { @MainActor in
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    animateView = true
+                }
+            }
         }
     }
 }
@@ -94,4 +169,17 @@ fileprivate struct PopOutOverlay<Header: View, Content: View>: View {
 
 #Preview {
     ContentView()
+}
+
+
+
+extension View {
+    func solidBackground(color: Color, opacity: CGFloat) -> some View {
+        Rectangle()
+            .fill(.background)
+            .overlay {
+                Rectangle()
+                    .fill(color.opacity(opacity))
+            }
+    }
 }
